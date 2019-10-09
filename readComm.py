cc37073cb820b0dd3de4462e668dc26d7598eb6e
Shawn V <liwyi@outlook.com>
2019-10-09 12:27:05 +0800

    Update readComm
    Add the function of reading the acknowledge-needed communications.

--- readComm.py
+++ readComm.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from tkinter import *
 import tkinter.messagebox as messagebox
-
+import os
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -21,8 +21,11 @@ class Application(Frame):
         self.label2.grid(row=1, column=0,padx=6, sticky=E)
         self.button = Button(self, text='Submit',width=7, borderwidth=1, relief=RIDGE, command=self.login)
         self.button.grid(row=0, column=2, sticky=E, padx=10, pady=10)
-        self.button2 = Button(self, text='Cancel', width=7, borderwidth=1, relief=RIDGE, command=self.quit)
+        self.button2 = Button(self, text='Cancel', width=7, borderwidth=1, relief=RIDGE, command=self.cancel)
         self.button2.grid(row=1, column=2, sticky=E, padx=10, pady=10)
+
+    def cancel(self):
+        os._exit(0)
 
     def login(self):
         loginID = self.nameInput.get()
@@ -41,14 +44,14 @@ def read(data):
         ccms = BeautifulSoup(session.get('https://www.wblt.ccms.teleperformance.com/ccms-bin/home.pl').content.decode('utf-8'),'lxml').find(class_='pmc').find(class_='ident').string
     except:
         messagebox.showinfo('CCMS', 'LoginID或密码错误！')
-        exit(0)
+        os._exit(0)
     url = 'https://www.wblt.ccms.teleperformance.com/ccms-bin/employee/communication.pl?employee_ident='+ccms
     resp = session.get(url)
     html = resp.content.decode('utf-8')
     soup = BeautifulSoup(html, 'lxml')
     if not soup.find(id = 'priority').tbody.find(class_='subject'):
         messagebox.showinfo('CCMS','无未读传讯！')
-        exit(0)
+        os._exit(0)
     list = soup.find(id='priority').tbody.find_all('tr')
     msgbox = ''
     count = 1
@@ -57,13 +60,18 @@ def read(data):
                 ident = item.find(class_='ident').string
                 itemType = item.find(class_='comm_type').string
                 subject = item.find(class_='subject').string
+                acknowledge = item.find(class_='ack_by').string
                 date = item.find(class_='date_issued').string
                 url_ident = 'https://www.wblt.ccms.teleperformance.com/ccms-bin/employee/communication.pl?frmTarget=NEW_COMMUNICATION&employee_ident='+ccms+'&ident='+ident
-                if session.get(url_ident):
-                    msgbox = msgbox + '%d'%count + '.' +subject+' | Date: '+date + '\n'
-                    count += 1
+                url_ident_ack = 'https://www.wblt.ccms.teleperformance.com/ccms-bin/employee/communication.pl?frmTarget=NEW_COMMUNICATION&new_communication=1&employee_ident='+ccms+'&ident='+ident+'&frmOption=ACK'
+                if acknowledge == 'N/A':
+                	session.get(url_ident)
+                else:
+                	session.get(url_ident_ack)
+                msgbox = msgbox + '%d'%count + '.' +subject+' | Date: '+date + '\n'
+                count += 1
     messagebox.showinfo('已读传讯', msgbox)
-    exit(0)
+    os._exit(0)
 
 
 app = Application()
